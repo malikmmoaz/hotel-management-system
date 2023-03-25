1a38f924d3f442f033f100f31ce7c19bdda72967
SABAINAKASHIF <sabaina.kashif2001@gmail.com>
2023-03-25 14:56:38 +0500

    Forget Password Implemented

--- hms/hotel/urls.py
+++ hms/hotel/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,4 +9,9 @@ urlpatterns = [
     path('login/', views.loginCustomer, name='login'),
     path('logout/', views.logoutCustomer, name='logout'),
     path('change_password/', views.change_password, name='change_password'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"),name='password_reset'), #the document says its password_reset tho
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),name='password_reset_done'),
+    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),name='password_reset_complete'),
+    
 ] 
