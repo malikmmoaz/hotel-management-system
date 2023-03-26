201934637a32d150084069016160bfdf240c58d0
Abdul Rafay Ahmed Khan <rafayak2002@gmail.com>
2023-03-27 00:09:41 +0500

    HotelManager model created

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
-    path('register/', views.registerCustomer, name='register'),
-    path('login/', views.loginCustomer, name='login'),
-    path('logout/', views.logoutCustomer, name='logout'),
+    path('register/', views.registerHotel, name='register'),
+    path('login/', views.loginHotel, name='login'),
+    path('logout/', views.logoutHotel, name='logout'),
     path('change_password/', views.change_password, name='change_password'),
     path('hotel_application/', views.hotel_application, name='hotel_application'),
 ] 
