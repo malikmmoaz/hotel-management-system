42a532aecefe79c0f476337aa96d4662f6a2bb8c
Abdul Rafay Ahmed Khan <rafayak2002@gmail.com>
2023-04-03 21:20:13 +0500

    Guest signup, login, change password and logout done

--- guest/models.py
+++ guest/models.py
@@ -1,3 +1,11 @@
+# import django models
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Guest(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'Guest: {self.user.first_name}'
