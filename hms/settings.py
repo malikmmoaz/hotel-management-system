dcf3ebaad3f66113e97fb0358579d3dd831e825f
Malik Muhammad Moaz <40146994+TheHotatoPotato@users.noreply.github.com>
2023-04-14 09:55:03 +0500

    static files for development setup

--- hms/settings.py
+++ hms/settings.py
@@ -120,19 +120,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
-
-MEDIA_ROOT = BASE_DIR / "staticfiles-cdn" / "uploads"
+#  https://hms-se.fra1.digitaloceanspaces.com
