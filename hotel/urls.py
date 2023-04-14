0ab1d3ab421905c828e15e7a49c98c40f2c4e063
Malik Muhammad Moaz <40146994+TheHotatoPotato@users.noreply.github.com>
2023-04-15 04:05:13 +0500

    pushed changes from rafay

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('hotel/', views.home, name='home'),
@@ -20,6 +21,10 @@ urlpatterns = [
     path('update_hotel_images/', views.update_hotel_images, name='update_hotel_images'),
     path('update_room_types/', views.update_room_types, name='update_room_types'),
     path('update_rooms/', views.update_rooms, name='update_rooms'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"),name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),name='password_reset_done'),
+    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"),name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),name='password_reset_complete'),
 ] 
 
 htmx_urlpatterns = [
