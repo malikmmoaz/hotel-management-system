2f947b894a35904443d8ab2b335780ce310ee9c8
Malik Muhammad Moaz <40146994+TheHotatoPotato@users.noreply.github.com>
2023-04-14 11:46:11 +0500

    connected signup login frontend, navbar

--- guest/urls.py
+++ guest/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('g', views.home, name='home'),
-    path('ghome/', views.home, name='home'),
-    path('gregister/', views.registerGuest, name='register'),
-    path('glogin/', views.loginGuest, name='login'),
+    # path('g', views.home, name='home'),
+    # path('ghome/', views.home, name='home'),
+    # path('gregister/', views.registerGuest, name='register'),
+    # path('glogin/', views.loginGuest, name='login'),
     path('glogout/', views.logoutGuest, name='logout'),
     path('gchange_password/', views.change_password, name='change_password'),
     path('hotel_listing/', views.hotel_listing, name='hotel_listing'),
